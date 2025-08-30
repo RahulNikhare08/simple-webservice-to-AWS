4dfad2de92f2c187835f4632a77260630ce692e4
RahulNikhare08 <99889832+RahulNikhare08@users.noreply.github.com>
2025-08-30 09:32:23 +0000

    destroy.tf added

--- infra/alb.tf
+++ infra/alb.tf
@@ -41,15 +41,12 @@ target_group_arn = aws_lb_target_group.tg.arn
 
 # Optional HTTP -> HTTPS redirect
 resource "aws_lb_listener" "http" {
-load_balancer_arn = aws_lb.app.arn
-port = 80
-protocol = "HTTP"
-default_action {
-type = "redirect"
-redirect {
-port = "443"
-protocol = "HTTPS"
-status_code = "HTTP_301"
-}
-}
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
 }
